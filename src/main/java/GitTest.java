0359296f9af8024e0628941d29cd93c916f14e83
zkj <m13155319729@163.com>
2022-08-26 16:01:58 +0800

    git提交测试3

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public void test() {
         System.out.println("git测试");
         System.out.println("true = " + true);
+        System.out.println("false = " + false);
     }
 }
